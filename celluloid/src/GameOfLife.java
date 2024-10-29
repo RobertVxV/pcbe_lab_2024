88b4b25841408dff8747a42d91929db1eed76b80
Viktor Ungur <viktor.ungur@student.upt.ro>
2024-10-29 17:55:23 +0100

    semaphores + comments

--- celluloid/src/GameOfLife.java
+++ celluloid/src/GameOfLife.java
@@ -1,41 +1,83 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 class FoodPool {
     private int totalFood;
+    private final Semaphore semaphore;
 
     public FoodPool(int initialFood) {
         this.totalFood = initialFood;
+        this.semaphore = new Semaphore(1);
     }
 
-    public synchronized int consumeFood(int amount) { // use semaphores
-        if (totalFood >= amount) {
-            totalFood -= amount;
-            return amount;
-        } else {
-            int availableFood = totalFood;
-            totalFood = 0;
-            return availableFood;
+    public int consumeFood(int amount) { // semafoare
+        int foodConsumed = 0;
+        try
+        {
+            semaphore.acquire();
+            if (totalFood >= amount) {
+                foodConsumed = amount;
+                totalFood -= amount;
+            } else {
+                foodConsumed = totalFood;
+                totalFood = 0;
+            }
+        }
+        catch(InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+        finally
+        {
+            semaphore.release();
+        }
+
+        return foodConsumed;
+    }
+
+    public void addFood(int amount) {
+        try
+        {
+            semaphore.acquire();
+            totalFood += amount;
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+        finally
+        {
+            semaphore.release();
         }
     }
 
-    public synchronized void addFood(int amount) {
-        totalFood += amount;
-    } //use semaphores
-
     public synchronized int getTotalFood() {
-        return totalFood;
+        int food;
+        try
+        {
+            semaphore.acquire();
+            food = totalFood;
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+            food = 0;
+        }
+        finally
+        {
+            semaphore.release();
+        }
+        return food;
     }
 }
 
 abstract class Cell extends Thread {
-    /*
-    these info should be gathered from a config file
-    */
-    protected static final int T_FULL = 5000;
-    protected static final int T_STARVE = 3000;
-    protected static final int REPRODUCTION_THRESHOLD = 10;
+    protected static final int T_FULL = 4000; // Time a cell stays full
+    protected static final int T_STARVE = 2000; // Time a cell can starve
+    protected static final int T_STARVE_VARIANCE = 1000;
+    protected static final int REPRODUCTION_THRESHOLD = 10; // store in json
 
     protected FoodPool foodPool;
     protected int mealsEaten = 0;
@@ -56,8 +98,10 @@ abstract class Cell extends Thread {
                 if (food > 0) {
                     eat(food);
                 } else {
+                    Random rand = new Random();
+                    int starvationTime = T_STARVE + rand.nextInt(T_STARVE_VARIANCE);
                     try {
-                        Thread.sleep(T_STARVE);
+                        Thread.sleep(starvationTime); // Starvation time
                         if (hungry) {
                             die();
                         }
@@ -75,7 +119,7 @@ abstract class Cell extends Thread {
         System.out.println(this.getName() + " ate food. Total meals: " + mealsEaten);
 
         try {
-            Thread.sleep(T_FULL);
+            Thread.sleep(T_FULL); // Stay full for some time
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -84,11 +128,11 @@ abstract class Cell extends Thread {
             reproduce();
         }
 
-        hungry = true;
+        hungry = true; // Become hungry again
     }
 
     protected void die() {
-        alive = false; //add an event that ends the tread
+        alive = false;
         System.out.println(this.getName() + " has died.");
         Random rand = new Random();
         int foodToAdd = rand.nextInt(5) + 1;
@@ -96,9 +140,9 @@ abstract class Cell extends Thread {
         System.out.println(this.getName() + " died and added " + foodToAdd + " food to the pool.");
     }
 
-    protected void kill()
+    protected void kill() // add an event that ends the thread
     {
-        alive = false; //add an event that ends  the thread
+        alive = false;
     }
 }
 
@@ -150,19 +194,22 @@ class SexualCell extends Cell {
     }
 }
 
-public class GameOfLife {
+public class Celluloid {
     public static void main(String[] args) {
-        int initialFood = 100;
+        int initialFood = 3;
         FoodPool foodPool = new FoodPool(initialFood);
 
+        // Create some initial cells
         AsexualCell asexualCell1 = new AsexualCell(foodPool);
         SexualCell sexualCell1 = new SexualCell(foodPool);
         SexualCell sexualCell2 = new SexualCell(foodPool);
 
+        // Start the simulation
         asexualCell1.start();
         sexualCell1.start();
         sexualCell2.start();
 
+        // Allow sexual cells to find partners
         sexualCell1.findPartner(sexualCell2);
     }
 }
