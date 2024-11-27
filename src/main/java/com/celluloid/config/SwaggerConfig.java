5237c30ca4f688d194a8e6417905028ffb133d96
RobertVxV <robertvinitchi@gmail.com>
2024-11-27 23:54:26 +0200

    initial setup for swagger

---
+++ src/main/java/com/celluloid/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.celluloid.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Game of Life API")
+                        .version("1.0")
+                        .description("API documentation for the Game of Life simulation"));
+    }
+}
--- src/main/java/com/celluloid/controller/ConfigController.java
+++ src/main/java/com/celluloid/controller/ConfigController.java
@@ -1,9 +1,12 @@
 package com.celluloid.controller;
 import com.celluloid.Config;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Configuration", description = "Endpoints for accessing application configuration")
 public class ConfigController {
     private final Config config;
 
@@ -12,6 +15,10 @@ public class ConfigController {
     }
 
     @GetMapping("/config")
+    @Operation(
+            summary = "Get configuration",
+            description = "Returns the current application configuration settings."
+    )
     public Config getConfig() {
         return config;
     }
