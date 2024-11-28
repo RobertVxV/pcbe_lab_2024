df75e852fd89ccc38a4505d8a08de56853c10982
RobertVxV <robertvinitchi@gmail.com>
2024-11-28 11:33:11 +0200

    added tag to stats endpoint

--- src/main/java/com/celluloid/controller/InfoController.java
+++ src/main/java/com/celluloid/controller/InfoController.java
@@ -2,6 +2,7 @@ package com.celluloid.controller;
 
 import com.celluloid.GlobalState;
 import com.celluloid.Watcher;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@Tag(name = "Information", description = "Endpoints for accessing application runtime information.")
 @RequestMapping("/info")
 public class InfoController {
 
